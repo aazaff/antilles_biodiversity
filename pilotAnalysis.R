d7d90b7d7a2961bb44fed86db537cec5e03fe2a5
Andrew Zaffos <aazaff@gmail.com>
2017-08-03 08:26:07 -0700

    Update pilotAnalysis.R

--- pilotAnalysis.R
+++ pilotAnalysis.R
@@ -69,7 +69,83 @@ CanonicalOBIS<-velociraptr::cleanTaxonomy(CanonicalOBIS,"genus_name")
 #############################################################################################################
 ################################################ LDG PLOT, CHIPS ############################################
 #############################################################################################################
-# No functions at this time
+# Steve Holland's optimized SQS function, with error bars added
+subsampleEvenness<-function(Abundance,Quota=0.9,Trials=100,IgnoreSingletons=FALSE,ExcludeDominant=FALSE) {
+	Abundance<-Abundance[Abundance>0]
+	if ((Quota <= 0 || Quota >= 1)) {
+		stop('The SQS Quota must be greater than 0.0 and less than 1.0')
+		}
+ 	# compute basic statistics
+	Specimens<-sum(Abundance)
+	NumTaxa<-length(Abundance)
+	Singletons<-sum(Abundance==1)
+	Doubletons<-sum(Abundance==2)
+	Highest<-max(Abundance)
+	MostFrequent<-which(Abundance==Highest)[1]
+ 	if (ExcludeDominant==FALSE) {
+		Highest<-0
+		MostFrequent<-0
+		}
+ 	# compute Good's u
+	U<-0
+	if (ExcludeDominant==TRUE) {
+		U<-1-Singletons/(Specimens-Highest)
+		} 
+	else {
+		U<-1-Singletons/Specimens
+		}
+ 	if (U==0) {
+		stop('Coverage is zero because all taxa are singletons')
+		}
+ 	# re-compute taxon frequencies for SQS
+	FrequencyInitial<-Abundance-(Singletons+Doubletons/2)/NumTaxa
+	Frequency<-FrequencyInitial/(Specimens-Highest)
+ 	# return if the quorum target is higher than estimated coverage
+	if ((Quota>sum(Frequency)) || (Quota >= sum(Abundance))) {
+		stop('SQS Quota is too large, relative to the estimated coverage')
+		}
+ 	# create a vector, length equal to total number of specimens,
+	# each value is the index of that species in the Abundance array
+	IDS<-unlist(mapply(rep,1:NumTaxa,Abundance))
+ 	# subsampling trial loop
+	Richness<-rep(0,Trials) # subsampled taxon richness
+	for (Trial in 1:Trials) {
+		Pool<-IDS # pool from which specimens will be sampled
+		SpecimensRemaining<- length(Pool) # number of specimens remaining to be sampled
+		Seen<-rep(0,NumTaxa) # keeps track of whether taxa have been sampled
+		SubsampledFrequency<-rep(0,NumTaxa) # subsampled frequencies of the taxa
+		Coverage<-0
+ 		while (Coverage<Quota) {
+			# draw a specimen
+			DrawnSpecimen<-sample(1:SpecimensRemaining,size=1)
+			DrawnTaxon<-Pool[DrawnSpecimen]
+ 			# increment frequency for this taxon
+			SubsampledFrequency[DrawnTaxon]<-SubsampledFrequency[DrawnTaxon]+1
+ 			# if taxon has not yet been found, increment the coverage
+			if (Seen[DrawnTaxon]==0) {
+				if (DrawnTaxon!=MostFrequent&&(IgnoreSingletons==0||Abundance[DrawnTaxon]>1)) {
+					Coverage<-Coverage+Frequency[DrawnTaxon]
+					}
+				Seen[DrawnTaxon]<-1
+ 				# increment the richness if the Quota hasn't been exceeded,
+				# and randomly throw back some draws that put the coverage over Quota
+				if (Coverage<Quota || runif(1)<=Frequency[DrawnTaxon]) {
+					Richness[Trial]<-Richness[Trial]+1
+					} 
+				else {
+					SubsampledFrequency[DrawnTaxon]<-SubsampledFrequency[DrawnTaxon]-1
+					}
+				}
+ 			# decrease pool of specimens not yet drawn
+			Pool[DrawnSpecimen]<-Pool[SpecimensRemaining]
+			SpecimensRemaining<-SpecimensRemaining-1
+			}
+		}
+ 	# compute subsampled richness
+	S2<-Richness[Richness>0]
+	SubsampledRichness<-exp(mean(log(S2)))*length(S2)/length(Richness)
+	return(c(round(SubsampledRichness,1),quantile(S2,0.975),quantile(S2,0.025)))
+	}
 
 ############################################# LDG SCRIPT, CHIPS #############################################
 # Break data into 1 degree bins (this could be done with just floor(), but round_any is more general)
@@ -81,11 +157,22 @@ SizeOBIS<-apply(AbundanceOBIS,2,sum)
 AbundanceOBIS<-AbundanceOBIS[,-which(SizeOBIS<1000)]
 
 # Calculate the SQS
-BiodiversityOBIS<-pbapply(AbundanceOBIS,2,velociraptr::subsampleEvenness,0.75)
+BiodiversityOBIS<-pbapply(AbundanceOBIS,2,subsampleEvenness,0.75)
 
 # Make the basic plot
-plot(y=BiodiversityOBIS,x=as.numeric(names(BiodiversityOBIS)),pch=16,cex=1.5,las=1,xlab="latitude",ylab="standardized richness",xaxs="i",yaxs="i",xlim=c(-90,90),ylim=c(0,525))
-abline(v=22)
+quartz(width=9,height=6)
+par(oma=c(1.5,0.5,0.5,0),mar=c(3,3,2,0.5),mgp=c(1.5,0.5,0))
+plot(y=BiodiversityOBIS[1,],x=as.numeric(colnames(BiodiversityOBIS)),pch=16,cex=1.5,las=1,xlab="latitude",ylab="standardized richness",xaxs="i",yaxs="i",xlim=c(-90,90),ylim=c(0,525))
+arrows(x0=as.numeric(colnames(BiodiversityOBIS)),y0=BiodiversityOBIS[2,],x1=as.numeric(colnames(BiodiversityOBIS)),y1=BiodiversityOBIS[3,],code=0)
+abline(v=17); abline(v=19);
+
+# Make a density plot
+quartz(width=9,height=6)
+par(oma=c(1.5,0.5,0.5,0),mar=c(3,3,2,0.5),mgp=c(1.5,0.5,0))
+DistributionOBIS<-rep(as.numeric(colnames(BiodiversityOBIS)),times=ceiling(BiodiversityOBIS[1,]))
+plot(density(DistributionOBIS),type="n",xlab="latitude",xlim=c(-90,90),ylab="kernel density of standardized richness",xaxs="i",yaxs="i")
+polygon(density(DistributionOBIS),col="cyan",lty=0)
+abline(v=17); abline(v=19)
 
 #############################################################################################################
 ############################################ ADP FUNCTIONS, CHIPS ###########################################
@@ -107,6 +194,9 @@ TaxonBeta<-transform(merge(taxonBeta(cullMatrix(LiveAssemblage,1,1)),TaxonBeta,b
 colnames(TaxonBeta)<-c("Live","B","C","D")
                   
 # Find the correlation matrix for the beta diversity
-cor(TaxonBeta,use="pairwise.complete.obs") 
+cor(TaxonBeta,use="pairwise.complete.obs")
+                  
+# Plot the overall beta diversity consistency
+
                   
                   
